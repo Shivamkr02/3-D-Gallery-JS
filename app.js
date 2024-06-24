d9eee7abf54343c1e98649f351fb6aab6d79939f
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-06-24 20:12:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const ImageElement = document.querySelector(".image-container");
+
+const prevEle = document.getElementById("prev");
+const nextEle =document.getElementById("next");
+
+let x=0;
+let timer;
+
+function updateGallery(){
+    ImageElement.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+
+    timer = setTimeout(()=>{
+        x = x-45;
+        updateGallery();
+    },1000)
+}
+
+updateGallery();
+
+nextEle.addEventListener("click",()=>{
+    x=x+45;
+    clearTimeout(timer);
+    updateGallery();
+})
+
+prevEle.addEventListener("click",()=>{
+    x=x-45;
+    clearTimeout(timer);
+    updateGallery();
+});
